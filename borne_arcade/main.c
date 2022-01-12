a2200b01cf4e6b3d8ae03fcaddb0f50d26962518
Chris <7072goldfish@gmail.com>
2022-01-12 10:58:53 +0100

    Premiere version d'un menu

--- borne_arcade/main.c
+++ borne_arcade/main.c
@@ -2,12 +2,60 @@
 #include <stdlib.h>
 #include "pendu.h"
 #include "mastermind.h"
+#include "juste_prix.h"
+
+void printMenu() {
+    printf("\tBienvenue sur la borne d'arcade !\n\n");
+    printf("Voici la liste des jeux : \n");
+    printf("1) Pierre-Feuille-Ciseaux\n");
+    printf("2) Juste Prix\n");
+    printf("3) Mastermind\n");
+    printf("4) Pendu\n");
+    printf("5) Quitter\n\n");
+    printf("Choississez a quel jeu vous allez jouer (Entrez un nombre entre 1 et 5) : ");
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    //printf("Hello world!\n");
     //playPendu();
-    playMastermind();
+    //playMastermind();
     //playJustePrix();
+
+    int quit = 0, choice = 0;
+
+    while(quit == 0) {
+        printMenu();
+        scanf("%d", &choice);
+        fflush(stdin);
+        printf("\n\n\n");
+        switch(choice) {
+        case 1:
+            printf("Le pierre-feuille-ciseaux n'est pas encore implemente. Revenez plus tard\n");
+            printf("\n\n\n");
+            break;
+        case 2:
+            playJustePrix();
+            printf("\n\n\n");
+            break;
+        case 3:
+            playMastermind();
+            printf("\n\n\n");
+            break;
+        case 4:
+            playPendu();
+            printf("\n\n\n");
+            break;
+        case 5:
+            quit = 1;
+            break;
+        default:
+            printf("Vous devez entrer un nombre entre 1 et 5 !\n\n\n");
+            break;
+        }
+    }
+
+    printf("Au revoir !");
+
     return 0;
 }
