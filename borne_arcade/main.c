396e1c9c898b6b05069abeb9e9dfa047de355fbd
Chris <7072goldfish@gmail.com>
2022-01-12 16:43:07 +0100

    Affichage high scores dans main

--- borne_arcade/main.c
+++ borne_arcade/main.c
@@ -13,20 +13,51 @@ void printMenu() {
     printf("2) Juste Prix\n");
     printf("3) Mastermind\n");
     printf("4) Pendu\n");
-    printf("5) Score\n\n");
-    printf("6) Quitter\n");
+    printf("5) Score\n");
+    printf("6) Quitter\n\n");
     printf("Choississez a quel jeu vous allez jouer (Entrez un nombre entre 1 et 5) : ");
+}
+
+void getHighScores() {
+    int choix = 0;
+    hightScore highScoresList[TAILLE_LISTE];
+    FILE* pt_fichier = NULL;
+    printf("Liste des jeux : \n");
+    printf("1) Pierre-Feuille-Ciseaux\n");
+    printf("2) Juste Prix\n");
+    printf("3) Mastermind\n");
+    printf("4) Pendu\n");
+    printf("Pour quel jeu voulez-vous voir le high score ? ");
+    scanf("%d", &choix);
+    switch(choix) {
+        case 1:
+            pt_fichier = loadGameHighScores("datas/PenduHighScore.sav", highScoresList);
+            break;
+        case 2:
+            pt_fichier = loadGameHighScores("datas/PenduHighScore.sav", highScoresList);
+            break;
+        case 3:
+            pt_fichier = loadGameHighScores("datas/MastermindHighScore.sav", highScoresList);
+            break;
+        case 4:
+            pt_fichier = loadGameHighScores("datas/PenduHighScore.sav", highScoresList);
+            break;
+        default:
+            printf("Selection invalide\n");
+            return;
+    }
+    if(!pt_fichier) {
+        return;
+    }
+    afficherHightScore(highScoresList);
+
+    fclose(pt_fichier);
+
 }
 
 
 int main()
 {
-    //printf("Hello world!\n");
-    //playPendu();
-    //playMastermind();
-    //playJustePrix();
-    //playPapierCiseaux();
-
 
     int quit = 0, choice = 0;
 
@@ -53,7 +84,7 @@ int main()
             printf("\n\n\n");
             break;
         case 5:
-            //afficherHightScore(highScoresList);
+            getHighScores();
             printf("\n\n\n");
             break;
         case 6:
